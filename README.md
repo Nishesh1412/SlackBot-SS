d43d384d49b02845f071bcccd6ddd209b14efe96
Nishesh Shukla <nish3996@colorado.edu>
2017-02-23 18:03:41 -0800

    Changed name, changed/added commands

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Temp bot name: SlackBot SS
 
 Instructions: 
-
+Install pip
+Install slackclient
+Install virtualenv
 Run the starterbot.py file by this command "python starterbot.py"
--- starterbot.py
+++ starterbot.py
@@ -3,12 +3,12 @@ import time
 from slackclient import SlackClient
 
 
-# starterbot's ID as an environment variable
+# WhitePages's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
+INITIAL_COMMAND = "info:"
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -20,13 +20,15 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+  
+    if command.startswith(INITIAL_COMMAND):
+            response = command + "Sure! Gathering information..."
+    else:
+            response = "Not sure what you mean. Use the following command to gather informationa bout a specific user on the team -> *" + INITIAL_COMMAND + "* *username*"
+
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
-
+    
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -47,7 +49,7 @@ def parse_slack_output(slack_rtm_output):
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
+        print("WhitePages connected and running!")
         while True:
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
